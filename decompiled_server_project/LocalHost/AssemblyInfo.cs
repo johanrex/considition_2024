abe4276bbb879c89ba2597070a906eef9f1035a9
Johan Rex <52hands@gmail.com>
2024-11-08 23:07:03 +0100

    ...

--- decompiled_server_project/LocalHost/AssemblyInfo.cs
+++ decompiled_server_project/LocalHost/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using System.Reflection;
 
+[assembly: Obfuscation(Feature = "encrypt resources", Exclude = false)]
+[assembly: Obfuscation(Feature = "rename symbol names with printable characters", Exclude = false)]
+[assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
 [assembly: AssemblyCompany("LocalHost")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
