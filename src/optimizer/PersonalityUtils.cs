ec18cddcf074ea505e6828d9a307407dbdd9c21d
Johan Rex <52hands@gmail.com>
2024-11-10 13:25:06 +0100

    ...

--- src/optimizer/PersonalityUtils.cs
+++ src/optimizer/PersonalityUtils.cs
@@ -7,32 +7,6 @@ namespace optimizer
 {
     public class PersonalityUtils
     {
-        /*
-        public static Personality StringToEnum(string personalityString)
-        {
-            if (!Enum.TryParse(personalityString, out Personality personalityEnum))
-            {
-                throw new Exception($"Can't find matching enum for personality {personalityString}.");
-            }
-            return personalityEnum;
-        }
-
-        public static Dictionary<Personality, PersonalitySpecification> ReadPersonalitiesFile(string personalitiesFile)
-        {
-            string jsonData = File.ReadAllText(personalitiesFile);
-
-            var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, PersonalitySpecification>>>(jsonData);
-            if (!jsonObject.ContainsKey("Personalities"))
-            {
-                throw new KeyNotFoundException("The key 'Personalities' was not found in the JSON data.");
-            }
-
-            var personalitiesJson = JsonConvert.SerializeObject(jsonObject["Personalities"]);
-            var personalitiesDict = JsonConvert.DeserializeObject<Dictionary<Personality, PersonalitySpecification>>(personalitiesJson);
-            return personalitiesDict;
-        }
-        */
-
         public static bool HasKnownPersonalities(Map map, Dictionary<Personality, PersonalitySpecification> personalities)
         {
             foreach (var customer in map.Customers)
